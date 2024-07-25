b4d100aa5f821ef11dbe3b458a1c571f5bd8dee7
Digbijayinee Clara <52991229+DigbijayineeClara07@users.noreply.github.com>
2024-07-25 17:46:06 +0530

    Update eks.tf

--- eks.tf
+++ eks.tf
@@ -1,52 +1,15 @@
-provider "aws" {
-  region = "eu-central-1"  # Specify your desired AWS region
-}
-
-# Create a new VPC for EKS cluster
-resource "aws_vpc" "eks_vpc" {
-  cidr_block = "10.0.0.0/16"
-  enable_dns_hostnames = true
-}
-
-# Create an Internet Gateway for the VPC
-resource "aws_internet_gateway" "eks_igw" {
-  vpc_id = aws_vpc.eks_vpc.id
-}
-
-# Create a subnet for EKS
-resource "aws_subnet" "eks_subnet" {
-  vpc_id     = aws_vpc.eks_vpc.id
-  cidr_block = "10.0.1.0/24"
-}
-
-# Create an EKS cluster
-module "eks_cluster" {
-  source  = "terraform-aws-modules/eks/aws"
-  version = "17.1.0"
-
-  cluster_name    = "my-eks-cluster"
-  cluster_version = "1.21"
+resource "aws_eks_cluster" "example" {
+  name     = "example"
   role_arn = arn:aws:iam::790543352839:role/eksClusterRole
 
-  vpc_id     = aws_vpc.eks_vpc.id
-  subnet_ids = [aws_subnet.eks_subnet.id]
-
-  node_groups = {
-    eks_nodes = {
-      desired_capacity = 2
-      max_capacity     = 2
-      min_capacity     = 1
-
-      instance_type = "t3.medium"
-    }
+  vpc_config {
+    subnet_ids = [subnet-08f9e3975049119b3, subnet-0a02cc68894da7f97, subnet-0a4f88b69332464b6]
   }
 
-  tags = {
-    Environment = "Development"
-    Owner       = "Terraform"
-  }
+output "endpoint" {
+  value = aws_eks_cluster.example.endpoint
 }
 
-output "kubeconfig" {
-  value = module.eks_cluster.kubeconfig
+output "kubeconfig-certificate-authority-data" {
+  value = aws_eks_cluster.example.certificate_authority[0].data
 }
