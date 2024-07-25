d9bdfb385111a9c1feb33c6ba25c0b78829d880c
Digbijayinee Clara <52991229+DigbijayineeClara07@users.noreply.github.com>
2024-07-25 17:05:16 +0530

    Update sg.tf

--- sg.tf
+++ sg.tf
@@ -2,47 +2,19 @@ provider "aws" {
   region = "eu-central-1"  # Specify your desired AWS region
 }
 
-resource "aws_emr_cluster" "example_cluster" {
-  name          = "example-emr-cluster"
-  release_label = "emr-6.5.0"  # Specify the EMR version you want to use
-  applications  = ["Spark", "Hadoop", "Hive", "Livy"]  # List of applications to be installed
+resource "aws_emrcontainers_virtual_cluster" "example" {
+  container_provider {
+    id   = aws_eks_cluster.example.name
+    type = "EKS"
 
-  service_role  = "AWSServiceRoleForEMRCleanup"  # Ensure this role exists
-  job_flow_role = "ec2-qa"  # Ensure this role exists
-
-  instance_group {
-    instance_role    = "MASTER"
-    instance_type    = "m5.xlarge"
-    instance_count   = 1
-    name             = "Master Instance Group"
-  }
-
-  instance_group {
-    instance_role    = "CORE"
-    instance_type    = "m5.xlarge"
-    instance_count   = 2
-    name             = "Core Instance Group"
-  }
-
-  configurations_json = <<EOF
-  [
-    {
-      "Classification": "spark",
-      "Properties": {
-        "maximizeResourceAllocation": "true"
+    info {
+      eks_info {
+        namespace = "default"
       }
     }
-  ]
-  EOF
-
-  bootstrap_action {
-    name = "Install Example Package"
-    path = "s3://your-bucket/bootstrap/install-example.sh"
-    args = ["arg1", "arg2"]
   }
 
-  tags = {
-    Environment = "Development"
-    Owner       = "Terraform"
-  }
+  name = "example"
 }
+
+
